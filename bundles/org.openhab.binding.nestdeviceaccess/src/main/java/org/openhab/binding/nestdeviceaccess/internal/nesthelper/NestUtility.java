83ae28d08c14648844bb1f3382521e23b6b181d6
brianhigginbothamtx@gmail.com <brianhigginbothamtx@gmail.com>
2020-10-29 21:00:09 -0500

    removed pubsub plumbing to get a good commit for prod

--- bundles/org.openhab.binding.nestdeviceaccess/src/main/java/org/openhab/binding/nestdeviceaccess/internal/nesthelper/NestUtility.java
+++ bundles/org.openhab.binding.nestdeviceaccess/src/main/java/org/openhab/binding/nestdeviceaccess/internal/nesthelper/NestUtility.java
@@ -19,12 +19,7 @@ import java.io.InputStreamReader;
 import java.text.ParseException;
 import java.text.SimpleDateFormat;
 import java.util.Calendar;
-import java.util.Collection;
-import java.util.Collections;
 import java.util.Date;
-import java.util.concurrent.ScheduledThreadPoolExecutor;
-import java.util.concurrent.TimeUnit;
-import java.util.concurrent.TimeoutException;
 
 import org.eclipse.smarthome.core.thing.Thing;
 import org.slf4j.Logger;
@@ -42,22 +37,8 @@ import com.google.api.client.http.HttpResponse;
 import com.google.api.client.http.HttpTransport;
 import com.google.api.client.http.javanet.NetHttpTransport;
 import com.google.api.client.json.jackson2.JacksonFactory;
-import com.google.api.gax.core.CredentialsProvider;
-import com.google.api.gax.core.FixedCredentialsProvider;
 import com.google.auth.oauth2.AccessToken;
-import com.google.auth.oauth2.GoogleCredentials;
-import com.google.auth.oauth2.OAuth2Credentials;
-import com.google.cloud.pubsub.v1.AckReplyConsumer;
-import com.google.cloud.pubsub.v1.MessageReceiver;
-import com.google.cloud.pubsub.v1.Subscriber;
-import com.google.cloud.pubsub.v1.SubscriptionAdminClient;
-import com.google.cloud.pubsub.v1.SubscriptionAdminSettings;
-import com.google.common.collect.Lists;
 import com.google.common.net.HttpHeaders;
-import com.google.pubsub.v1.ProjectName;
-import com.google.pubsub.v1.ProjectSubscriptionName;
-import com.google.pubsub.v1.PubsubMessage;
-import com.google.pubsub.v1.Subscription;
 
 /**
  * The {@link NestUtility} is general utility class to help with all Nest SDM APIs
@@ -307,76 +288,6 @@ public class NestUtility {
             return ((e.getMessage().substring(0, 3)));
         }
 
-    }
-
-    public static void listSubscriptionInProjectExample(String projectId) throws IOException {
-        GoogleCredentials credentials = GoogleCredentials.create(googleAccessToken)
-                .createScoped(Lists.newArrayList("https://www.googleapis.com/auth/cloud-platform"));
-
-        Collection<String> scopes = Collections.singleton("https://www.googleapis.com/auth/pubsub");
-
-        credentials.createScoped(scopes);
-        CredentialsProvider cred = FixedCredentialsProvider
-                .create(OAuth2Credentials.newBuilder().setAccessToken(googleAccessToken).build());
-
-        SubscriptionAdminSettings.Builder subscriptionAdminSettingsBuilder = SubscriptionAdminSettings.newBuilder();
-        subscriptionAdminSettingsBuilder.setCredentialsProvider(cred);
-        SubscriptionAdminSettings subscriptionAdminSettings = subscriptionAdminSettingsBuilder.build();
-
-        try (SubscriptionAdminClient subscriptionAdminClient = SubscriptionAdminClient
-                .create(subscriptionAdminSettings)) {
-            ProjectName projectName = ProjectName.of(projectId);
-            for (Subscription subscription : subscriptionAdminClient.listSubscriptions(projectName).iterateAll()) {
-                logger.debug("List Subscription {}", subscription.getName());
-            }
-            logger.debug("Listed all the subscriptions in the project.");
-        }
-    }
-
-    public static void pubSubEventHandler(String projectId, String subscriptionId) {
-        ProjectSubscriptionName subscriptionName = ProjectSubscriptionName.of(projectId, subscriptionId);
-
-        // Instantiate an asynchronous message receiver.
-        ScheduledThreadPoolExecutor executor = new ScheduledThreadPoolExecutor(1);
-        MessageReceiver receiver = new MessageReceiver() {
-            @Override
-            public void receiveMessage(PubsubMessage message, final AckReplyConsumer consumer) {
-                logger.debug("got message: {}", message.getData().toStringUtf8());
-                executor.schedule(new Runnable() {
-                    @Override
-                    public void run() {
-                        consumer.ack();
-                    }
-                }, 30, TimeUnit.SECONDS);
-            }
-        };
-        Subscriber subscriber = null;
-        try {
-
-            logger.debug("We are starting up the sub... subName {}", subscriptionName);
-            GoogleCredentials credentials = GoogleCredentials.create(googleAccessToken);
-
-            Collection<String> scopes = Collections.singleton("https://www.googleapis.com/auth/pubsub");
-
-            credentials.createScoped(scopes);
-            CredentialsProvider cred = FixedCredentialsProvider
-                    .create(OAuth2Credentials.newBuilder().setAccessToken(googleAccessToken).build());
-            subscriber = Subscriber.newBuilder(subscriptionName, receiver).setCredentialsProvider(cred).build();
-            // Start the subscriber.
-            logger.debug("Got the subscriber.. {}", subscriber.getSubscriptionNameString());
-            subscriber.startAsync().awaitRunning();
-            logger.debug("Listening for messages on {}", subscriptionName.toString());
-            // Allow the subscriber to run for 30s unless an unrecoverable error occurs.
-            subscriber.awaitTerminated(30, TimeUnit.SECONDS);
-        } catch (IllegalStateException e) {
-            logger.debug("illegal state exception {}", e.getMessage());
-
-        } catch (TimeoutException timeoutException) {
-            // Shut down the subscriber after 30s. Stop receiving messages.
-            logger.debug("Timedout exception {}", timeoutException.getMessage());
-            subscriber.stopAsync();
-
-        }
     }
 
     /*
